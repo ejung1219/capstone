9e1755edbd347ffc9ae4988d070c48277a6871c3
ejung1219 <ejung1219@naver.com>
2022-06-17 02:41:56 +0900

    model 수정 + upload file 수정

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from models import User
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 from config import db
-from s import ssd
-from modular import play
+#from s import ssd
+#from modular import play
 
 import uvicorn
 import os
@@ -46,7 +46,7 @@ async def create_upload_files(num : int, current_user:User = Depends(get_current
 
     if num != current_user.target_num:
         db["users"].update_one({'username': realname}, {'$set': {'target_num': num}})
-#
+
     for file in files:
         contents = await file.read()
         with open(os.path.join(UPLOAD_DIRECTORY, file.filename), "wb") as fp:
@@ -81,10 +81,11 @@ async def login(request:OAuth2PasswordRequestForm = Depends()):
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f'Wrong Username or password')
 	access_token = create_access_token(data={"sub": user["username"] })
 	return {"access_token": access_token, "token_type": "bearer"}
-
+""""
 @app.put("/play")
 async def algo(current_user:User = Depends(get_current_active_user)):
 #algorithm
+
     ssd()
     cnt = play()
     score = 0
@@ -100,14 +101,14 @@ async def algo(current_user:User = Depends(get_current_active_user)):
         score = 150
     else:
         score = 200
-
+...
 #update
     realname = current_user.username
     user_score = current_user.score + score
     db["users"].update_one({'username': realname}, {'$set': {'score': user_score}})
 
     return {"you get your cut!" : score}
-
+"""
 @app.get("/users/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
